2994efd09edb770ac077968082a0e8c4cf02de81
Neelesh Sharma <30874688+pniiitr@users.noreply.github.com>
2018-05-21 06:33:55 -0700

    Create homepage.js

---
+++ homepage.js
@@ -0,0 +1,55 @@
+
+//var audio1 = document.getElementById("sef1");
+/*function namaste()
+{
+
+d = new Date;
+n = d.getHours();
+if(0<=n<11)
+{
+    document.getElementById("namastae").innerHTML="Good Morning";
+}
+else if(12<=n<=15)
+{
+    document.getElementById("namastae").innerHTML="Good Afternoon";
+}
+else if(16<=n<=20)
+{
+    document.getElementById("namastae").innerHTML="Good Evening";
+}
+else{
+    document.getElementById("namastae").src="Have a nice Time"; 
+}
+}*/
+
+a0.onmouseover = function pictureChange()
+{ //audio1.play();
+document.getElementById('theImage').src="medium.png";
+}
+a1.onmouseover = function pictureChange()
+{//audio1.play();
+document.getElementById('theImage').src="dsc01367.jpg";
+}
+a2.onmouseover = function pictureChange()
+{//audio1.play();
+document.getElementById('theImage').src="robogames_16x9-3.jpg";
+}
+a3.onmouseover = function pictureChange()
+{//audio1.play();
+document.getElementById('theImage').src="a270b4f0af6596d7b0413bedd1eb6b26.jpg";
+}
+a4.onmouseover = function pictureChange()
+{//audio1.play();
+document.getElementById('theImage').src="1.jpg";
+}
+a5.onmouseover = function pictureChange()
+{//audio1.play();
+document.getElementById('theImage').src="DUST514.jpg";
+}
+function seff()
+{
+    var audio = document.getElementById("sef");
+
+    audio.play();
+    }
+    
